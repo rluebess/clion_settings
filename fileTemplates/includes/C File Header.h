f10e95d34f6c706d4f70cb0d9f89720db4032997
rl <rl@DELLICIOUS>
2020-05-29 12:30:45 +0200

    CL-2020.1.1 <rl@DELLICIOUS Create ide.general.xml, sshConfigs.xml, project.default.xml, diff.xml, laf.xml, Default.xml, cpp.toolchains.xml, find.xml, ui.lnf.xml, workspace.xml, wsl.distributions.xml, keymap.xml, colors.scheme.xml, KeyPromoterXSettings.xml, Windows remapped go to header_source Proper Redo.xml, editor.xml, Windows remapped go to header_source.xml, C File Header.h, webServers.xml, debugger.xml, updates.xml

---
+++ fileTemplates/includes/C File Header.h
@@ -0,0 +1,16 @@
+#if ($HEADER_COMMENTS)
+///*****************************************************************************
+/*
+|*  Copyright       :   (c) ${YEAR} CAS (Deutschland) 
+|*  Filename        :   ${FILE_NAME}
+|*  Version         :   0.1
+|*  Programmer(s)   :   Rolf Luebess (RL)
+|*  Created         :   ${DATE}
+|*  Modified        :
+|*  Description     :   CT100 GOBD Host file
+|*  
+|*  This file was created as part of the GOBD fiscal memory project
+*/
+//****************************************************************************///
+
+#end
